3a6b3510d9ee82dac72a78b6369cb4b2fb9e1d13
wurongqing <2213629594@qq.com>
2018-04-12 20:02:48 +0800

    完成！

--- 网易课堂/综合案例/JDShopCar.js
+++ 网易课堂/综合案例/JDShopCar.js
@@ -1,23 +1,33 @@
 new Vue({
-    el:"#app",
-    data:{
-        ShopList:[]
+    el: "#app",
+    data: {
+        ShopList: []
     },
     //组件加载完毕执行
-    mounted(){
+    mounted() {
         this.getShopCarData();
         console.log("开始")
     },
-    methods:{
+    methods: {
         //获取商品数据
-        getShopCarData(){
-            this.$http.jsonp('http://localhost:54121/Two.ashx',{jsonp:"callback"}).then(response => {
-                console.log(response.body)
-             
-              }, response => {
+        getShopCarData() {
+            // this.$http.jsonp('http://localhost:54121/Two.ashx',{jsonp:"callback"}).then(response => {
+            //     console.log(response.body)
+
+            //   }, response => {
+            //     // error callback
+            //     console.log(response.error)
+            //   });
+            // GET /someUrl
+            this.$http.get('./shopCar.json').then(response => {
+
+                // get body data
+                alert(response.data);
+                
+
+            }, response => {
                 // error callback
-                console.log(response.error)
-              });
+            })
         }
     }
 })
